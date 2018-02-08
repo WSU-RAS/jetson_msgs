04670014561f85ab2c77ed08282f0c5ed3877c78
Garrett Wilson <g@floft.net>
2018-02-08 23:10:09 +0000

    Updated go_to.py to newer one from other repo

--- src/go_to.py
+++ src/go_to.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python2
+"""
+Go To Object node
+
+This node listens to /go_to where you can publish an object name to tell the
+robot to go to the last location it saw this object.
+
+Example:
+    rostopic pub -1 /go_to std_msgs/String "glass"
+"""
 import json
 import rospy
 import actionlib
@@ -31,42 +40,51 @@ class GoToObject:
         rospy.spin()
     """
     def __init__(self):
+        self.running = False
+
         # Name this node
         rospy.init_node('goToObject')
+
+        # Cancel on shutdown
+        rospy.on_shutdown(self.shutdown)
+
+        # Tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+
+        # Allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+        rospy.loginfo("Waiting for the action server")
 
         # Listen to object locations that are published
         rospy.Subscriber("/go_to", String, self.callback_object)
 
     def goTo(self, x, y):
-        #tell the action client that we want to spin a thread by default
-        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        rospy.loginfo("wait for the action server to come up")
-        #allow up to 5 seconds for the action server to come up
-        self.move_base.wait_for_server(rospy.Duration(5))
-
-        #we'll send a goal to the robot to move 3 meters forward
         goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
         goal.target_pose.pose.position.x = x
         goal.target_pose.pose.position.y = y
-        goal.target_pose.pose.orientation.w = 1.0 #go forward
+        goal.target_pose.pose.orientation.w = 1.0 # Go forward
 
-        #start moving
+        # Start moving
+        self.running = True
         self.move_base.send_goal(goal)
 
-        #allow TurtleBot up to 60 seconds to complete task
+        # Allow TurtleBot up to 60 seconds to complete task
         success = self.move_base.wait_for_result(rospy.Duration(600))
+        self.running = False
 
         if not success:
             self.move_base.cancel_goal()
-            rospy.loginfo("The base failed to move forward 3 meters for some reason")
+            rospy.loginfo("Move base goal failed")
         else:
             # We made it!
             state = self.move_base.get_state()
 
             if state == GoalStatus.SUCCEEDED:
-                rospy.loginfo("Hooray, the base moved 3 meters forward")
+                rospy.loginfo("Move base goal succeeded")
+            else:
+                rospy.logwarn("Move base goal not success")
 
     def callback_object(self, data):
         name = data.data
@@ -74,10 +92,17 @@ class GoToObject:
 
         if locations and len(locations) > 0:
             location = locations[0]
-            rospy.loginfo("Going to object "+name+" x: " + str(location.x) + " y: " + str(location.y))
+            rospy.loginfo("Going to object "+name+" { x: " + str(location.x) + ", y: " + str(location.y) + " }")
             self.goTo(location.x, location.y)
         else:
             rospy.logerr("Cannot go to object "+name)
+
+    def shutdown(self):
+        rospy.loginfo("Stopping /go_to")
+
+        # Cancel if we are currently going somewhere
+        if self.running:
+            self.move_base.cancel_goal()
 
 if __name__ == '__main__':
     try:
